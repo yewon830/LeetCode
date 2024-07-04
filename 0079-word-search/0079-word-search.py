771e9a862dc2bf405be45b518e0dd4a01239c0b4
Ye Won Jeong <wonderwall00@naver.com>
2024-07-04 20:51:01 +0900

    Time: 4481 ms (37.81%), Space: 16.5 MB (83.14%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,29 +4,29 @@ class Solution:
         m = len(board[0]) # 가로
         visited = [[0]*m for _ in range(n)] # 방문 여부
         
-        def backTracking(cr,cc,w, visited):
+        def backTracking(cr,cc,w):
             #. 방문 안했고 글자가 같으면
             if not visited[cr][cc] and board[cr][cc] == word[w]:
-                if w ==len(word)-1:
-                    return True #찾았다
-                visited[cr][cc] = 1
-                flag = False
-                
+                visited[cr][cc] = 1  #방문 표시
+                if w ==len(word)-1: #단어를 찾았다
+                    return True
+
                 for dr, dc in [[-1,0],[1,0],[0,-1],[0,1]]:
                     # 이동 했을 때 범위 내인가?
                     if 0<=dr+cr<n and 0<=dc+cc<m:
                         nr = dr + cr
                         nc = dc + cc
-                        # 백트래킹 해서 잘 되고 있으면
-                        if backTracking(nr,nc,w+1, visited):
-                            flag = True
+                        if backTracking(nr,nc,w+1):
+                            return True #단어 발견
 
                 visited[cr][cc] = 0
-                return flag
+                    
+                return False
+            
         
         for i in range(n):
             for j in range(m):
-                if backTracking(i, j, 0, visited):
+                if backTracking(i, j, 0):
                     return True
 
         return False
