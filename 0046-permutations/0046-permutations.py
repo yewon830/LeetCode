6813b60bbce93a23b7c7fff0bc5af1bce1a3f185
Ye Won Jeong <wonderwall00@naver.com>
2024-07-05 19:15:52 +0900

    Time: 40 ms (60.51%), Space: 16.8 MB (6.48%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        #순열의 경우 1,2,3 1,3,2 가 다르다. 이전인덱스도 볼 수 있다.
         result = []
-        def backTracking(perm):
+        def backTracking(path):
+            # 베이스 케이스 : n개 다 채우면 끝
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
             
-            if len(perm) == len(nums):
-                result.append(perm[:])
+            for e in nums:
+                if(e not in path):
+                    path.append(e)
+                    backTracking(path)
+                    path.pop()
+                                
             
-            for i in range(len(nums)):
-                if(nums[i] not in perm):
-                    perm.append(nums[i])
-                    backTracking(perm)
-                    perm.pop()
-                
         backTracking([])
         return result
