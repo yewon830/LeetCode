8754da785d2191f8755bdcb05b666de7d7650ab3
Ye Won Jeong <wonderwall00@naver.com>
2024-07-05 19:51:37 +0900

    Time: 40 ms (34.74%), Space: 16.7 MB (24.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # 부분 집합은 중복은 허용 x 인데 중간 과정을 포함해야 한다.
         result = []
-        def backTracking(start, subsetList):
+        def backTracking(start,path):
             
-            result.append(subsetList[:])
-            
+            result.append(path[:])
             
             for i in range(start, len(nums)):
-                subsetList.append(nums[i])
-                backTracking(i+1, subsetList)
-                subsetList.pop()
-        backTracking(0, [])
+                path.append(nums[i])
+                backTracking(i+1, path)
+                path.pop()
+        backTracking(0,[])
         return result
+                
