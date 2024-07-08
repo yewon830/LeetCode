7bcb39c9c99eb13fb46ac7206c0599b73253f172
Ye Won Jeong <wonderwall00@naver.com>
2024-07-08 16:56:59 +0900

    Time: 4463 ms (38.36%), Space: 16.7 MB (13.31%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        n = len(board) # 세로
-        m = len(board[0]) # 가로
-        visited = [[0]*m for _ in range(n)] # 방문 여부
+        # 상하 좌우로 돌면서, 단어가 만들어지는지 백트래킹으로 체크
+        #  종료 조건 = [w가 마지막 인덱스 도달
+        # visited 활용해서 방문 요소 체크
+        n = len(board)
+        m = len(board[0])
+        visited = [[0] * m for _ in range(n)]
         
-        def backTracking(cr,cc,w):
-            #. 방문 안했고 글자가 같으면
-            if not visited[cr][cc] and board[cr][cc] == word[w]:
-                visited[cr][cc] = 1  #방문 표시
-                if w ==len(word)-1: #단어를 찾았다
+        def backTracking(i,j,w):
+            
+            if not visited[i][j] and board[i][j] == word[w]:
+                visited[i][j] = 1
+                if w == len(word)-1:
                     return True
-
-                for dr, dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-                    # 이동 했을 때 범위 내인가?
-                    if 0<=dr+cr<n and 0<=dc+cc<m:
-                        nr = dr + cr
-                        nc = dc + cc
-                        if backTracking(nr,nc,w+1):
-                            return True #단어 발견
-
-                visited[cr][cc] = 0               
-                return False
-            else:
-                return False
+                
+                for x,y in [[-1,0],[1,0],[0,1],[0,-1]]:
+                    if 0<=i+x<n and 0<= j+y<m: #범위 검사
+                        if backTracking(i+x, j+y, w+1):
+                            return True
+                visited[i][j] = 0
+            
+            return False
         
         for i in range(n):
             for j in range(m):
-                if backTracking(i, j, 0):
+                if backTracking(i,j,0):
                     return True
-
         return False
+                
+                            
+        
+        
+            
+
+            
                     
             
             
