a0ff7aa013e7a2c8af9cbc21f8c90d23e2a1d942
Ye Won Jeong <wonderwall00@naver.com>
2024-07-13 00:34:51 +0900

    Time: 31 ms (82.62%), Space: 16.6 MB (12.10%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,44 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # 짝을 맞춘다.
         stack = []
-        #s에 있는걸 pop한다
-        
-#         pop해서 나오는거랑 지금거랑 맞아야 함.
         for i in range(len(s)):
-            # 여는 괄호 없이 닫는 괄호만 나오는 경우 
-            if not stack and (s[i] == ')'or s[i] == '}' or s[i]== ']'):
+            if s[i] == '(':
+                stack.append(')')
+            elif s[i] == '[':
+                stack.append(']')
+            elif s[i] == '{':
+                stack.append('}') 
+            elif stack and stack[-1] == s[i]:
+                    stack.pop()
+            else:
                 return False
-            
-            if s[i] == '(' or s[i] == '[' or s[i] == '{':
-                stack.append(s[i]) 
-            if s[i] == ')':
-                if stack[-1] =='(':
-                    stack.pop()
-                else:
-                    return False
-            elif s[i] == ']':
-                if stack[-1] == '[':
-                    stack.pop()
-                else:
-                    return False
-            elif s[i] == '}':
-                if stack[-1] == '{':
-                    stack.pop()
-                else:
-                    return False
-
-        
         if stack:
             return False
         else:
             return True
-            
-            
-            
-            
-            
-        
-        
-                
-            
-            
