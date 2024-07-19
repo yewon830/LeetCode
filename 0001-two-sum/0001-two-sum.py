78559c846c7c3c93d83030daf3360d6f33de0c30
Ye Won Jeong <wonderwall00@naver.com>
2024-07-19 13:59:53 +0900

    Time: 54 ms (85.06%), Space: 18.1 MB (7.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,28 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #  투 포인터로 풀기
-        # 투 포인터로 풀기 위해서는 정렬이 필요한데, 정렬해버리면 원래 인덱스 값의 정보가 없어지므로
-        # enumerate로 인덱스 값을 남겨준다
-        new_nums = [i for i in enumerate(nums)]
-        
-        #정렬(1번째 키 기준)
-        new_nums.sort(key= lambda x : x[1])
-        
-        #투포인터 0이 인덱스 1이 값
+        # 인덱스 출력
+        new_nums = []
+        for i in enumerate(nums):
+            new_nums.append(i)
+        new_nums.sort(key = lambda x : x[1])
         left = 0
-        right = len(new_nums)-1
-        while(left<=right):
+        right = len(new_nums) -1
+        while left<=right:
             if new_nums[left][1] + new_nums[right][1] == target:
                 return [new_nums[left][0], new_nums[right][0]]
             elif new_nums[left][1] + new_nums[right][1] < target:
                 left += 1
             else:
                 right -= 1
-            
-                
-        
-                
-                
-                
-            
-        
