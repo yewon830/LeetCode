0b4fcc2adb4fa8aae10409747d79b74bd3fbe2f0
Ye Won Jeong <wonderwall00@naver.com>
2024-07-23 21:38:55 +0900

    Time: 246 ms (54.88%), Space: 18.8 MB (89.80%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,34 +2,28 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #사방으로 이동
-        #연결되어있으면 섬 col이 n 이고 row가 m
-        n = len(grid[0]) #col
-        m = len(grid) #row
-        cnt = 0;
-        visited = [[0]*n for _ in range(m)]
-        #여러 곳에서 시작해야. 
-        
+        n = len(grid)
+        m = len(grid[0])
+        #1이 섬
+        visited = [[0] *m for _ in range(n)]
         def bfs(i,j):
-            queue = deque()
-            queue.append([i,j])
+            queue = deque([(i,j)])
             visited[i][j] = 1
             while queue:
-                cr, cc = queue.popleft()
-                #수평 수직이동이므로 동서남북
-                for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
-                    nr = cr + dr
-                    nc = cc + dc
-                    #범위 검사 + 방문x + 그리드에서 섬이면
-                    if 0<=nr<m and 0<=nc<n and not visited[nr][nc] and grid[nr][nc] == "1":
-                        queue.append([nr,nc])
-                        visited[nr][nc] = 1
+                ci, cj = queue.popleft()
+                for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
+                    ni = dr + ci
+                    nj = dc + cj
+                    if 0<=ni<n and 0<=nj<m and grid[ni][nj] =="1" and not visited[ni][nj]:
+                        queue.append((ni,nj))
+                        visited[ni][nj] = 1        
         
-        
-        for i in range(m):
-            for j in range(n):
-                if not visited[i][j] and grid[i][j]=="1":
+        cnt = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1" and not visited[i][j]:
                     bfs(i,j)
                     cnt += 1
+                    
         return cnt
         
