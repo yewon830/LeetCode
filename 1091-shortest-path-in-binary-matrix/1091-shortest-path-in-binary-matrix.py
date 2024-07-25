a8a54a897b578cafec5fce5c96fbee3d0e6accdb
Ye Won Jeong <wonderwall00@naver.com>
2024-07-25 17:20:45 +0900

    Time: 411 ms (76.27%), Space: 16.8 MB (83.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,26 +2,27 @@ from collections import deque
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n= len(grid)
-        #0,0에서 시작해서 n-1,n-1로 끝남
-        # 만약 첫 시작이나 끝이 1이라면 그냥 -1
+        # 0만 이동
+        # 8방향 이동 n * n
+        n = len(grid)
+        
         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
-
-        queue = deque()
-        queue.append((0,0,1))
-
+        
+        queue = deque([(0,0,1)])
         visited = [[0]*n for _ in range(n)]
+        visited[0][0] = 1
         
         while queue:
-            cr,cc, cnt = queue.popleft()
+            cr,cc,cnt = queue.popleft()
             if cr == n-1 and cc == n-1:
+                #도착
                 return cnt
-            #8방향 이동
-            for dr,dc in [[0,1],[0,-1],[1,0],[-1,0],[-1,-1],[1,-1],[1,1],[-1,1]]:
+            for dr,dc in [[-1,0],[1,0],[0,1],[0,-1],[-1,-1],[-1,1],[1,-1],[1,1]]:
                 nr = dr + cr
                 nc = dc + cc
-                if 0<= nr< n and 0<= nc<n and grid[nr][nc] == 0 and not visited[nr][nc]:
+                # 범위 내, 노중복, 0
+                if 0<=nr<n and 0<=nc<n and not visited[nr][nc] and grid[nr][nc] == 0:
                     queue.append((nr,nc,cnt+1))
                     visited[nr][nc] = 1
         return -1
