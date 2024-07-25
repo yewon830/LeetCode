6d017ac255a2197c4a36d23b77fc42a71682d095
Ye Won Jeong <wonderwall00@naver.com>
2024-07-26 01:49:36 +0900

    Time: 369 ms (98.13%), Space: 17.5 MB (33.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,24 @@
 from collections import deque
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        #코인을... 누적으로 더해주면서 cnt 출력
         if amount == 0:
             return 0
-        # 이거 중복 가능..[1,2,5] 각각이  [1,2,5] 로 갈 수 있음.
-        # cnt 넘겨주기
-        queue = deque([(0,0)]) #현재금액, cnt
+        
+        queue = deque([(0,0)])
         visited = set()
-
+        
         while queue:
             cur_coin, cnt = queue.popleft()
             if cur_coin == amount:
                 return cnt
             for i in range(len(coins)):
                 next_coin = cur_coin + coins[i]
-                if next_coin not in visited and next_coin <= amount:
-                    queue.append((next_coin, cnt+1))
+                if next_coin not in visited and next_coin<= amount:
+                    queue.append((next_coin,cnt+1))
                     visited.add(next_coin)
         return -1
+        
                     
         
         
