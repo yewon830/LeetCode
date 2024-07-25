394a13cc9e024c5728c8c68cf0793ce069915c52
Ye Won Jeong <wonderwall00@naver.com>
2024-07-25 17:11:42 +0900

    Time: 239 ms (65.00%), Space: 18.8 MB (90.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,21 +2,24 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        #1이 섬, 섬 갯수 찾기
+        #bfs
         n = len(grid)
         m = len(grid[0])
-        #1이 섬
-        visited = [[0] *m for _ in range(n)]
+        visited = [[0]*m for _ in range(n)]
         def bfs(i,j):
             queue = deque([(i,j)])
             visited[i][j] = 1
+            
             while queue:
-                ci, cj = queue.popleft()
-                for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-                    ni = dr + ci
-                    nj = dc + cj
-                    if 0<=ni<n and 0<=nj<m and grid[ni][nj] =="1" and not visited[ni][nj]:
-                        queue.append((ni,nj))
-                        visited[ni][nj] = 1        
+                cr, cc = queue.popleft()
+                for dr, dc in [[-1,0],[1,0],[0,1],[0,-1]]:
+                    nr = dr + cr
+                    nc = dc + cc
+                    if 0<=nr<n and 0<=nc<m and not visited[nr][nc] and grid[nr][nc] == "1":
+                        queue.append((nr,nc))
+                        visited[nr][nc] = 1
+            
         
         cnt = 0
         for i in range(n):
@@ -24,6 +27,5 @@ class Solution:
                 if grid[i][j] == "1" and not visited[i][j]:
                     bfs(i,j)
                     cnt += 1
-                    
         return cnt
-        
+                    
