574194eacf2b88fc4fd48c4f12b0a21e200634fa
Ye Won Jeong <wonderwall00@naver.com>
2024-07-27 02:37:43 +0900

    Time: 4622 ms (33.85%), Space: 16.5 MB (83.26%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,41 +1,34 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # 상하 좌우로 돌면서, 단어가 만들어지는지 백트래킹으로 체크
-        #  종료 조건 = [w가 마지막 인덱스 도달
-        # visited 활용해서 방문 요소 체크
+        # 단어 인덱스를 늘려가면서 하나씩 비교
         n = len(board)
         m = len(board[0])
-        visited = [[0] * m for _ in range(n)]
-        
-        def backTracking(i,j,w):
-            
-            if not visited[i][j] and board[i][j] == word[w]:
-                visited[i][j] = 1
+        visited = [[0]*m for _ in range(n)]
+        def backTracking(r,c,w):
+            if board[r][c] == word[w] and not visited[r][c]:
+                visited[r][c] = 1
                 if w == len(word)-1:
                     return True
-                
-                for x,y in [[-1,0],[1,0],[0,1],[0,-1]]:
-                    if 0<=i+x<n and 0<= j+y<m: #범위 검사
-                        if backTracking(i+x, j+y, w+1):
-                            return True
-                visited[i][j] = 0
             
-            return False
+                for dr,dc in [[-1,0],[1,0],[0,1],[0,-1]]:
+                    nr = r + dr
+                    nc = c + dc
+                    if 0<=nr<n and 0<=nc<m :
+                        if backTracking(nr,nc,w+1):
+                            return True
+                visited[r][c] = 0
+       
         
         for i in range(n):
             for j in range(m):
-                if backTracking(i,j,0):
-                    return True
+                if board[i][j] == word[0] and not visited[i][j]:
+                    if backTracking(i,j,0):
+                        return True
         return False
-                
-                            
         
         
-            
 
             
-                    
-            
-            
-               
+        
+        
         
