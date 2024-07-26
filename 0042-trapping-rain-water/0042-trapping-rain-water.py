d4ef97cbc6db871b325f2546c862652609d9d272
Ye Won Jeong <wonderwall00@naver.com>
2024-07-26 15:56:47 +0900

    Time: 82 ms (99.21%), Space: 18.4 MB (43.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,29 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # 투포인터로 풀어보자
-        # 좌우에서 이동을 하는데, 왼쪽과 오른쪽을 비교해서 낮은 걸 기준으로 움직인다.
-        # 가장 높은 왼쪽, 가장 높은 오른쪽 기준을 세운다
-        # 이동하면서, 가장 높은거 - 지금을 cnt에 더한다
+        # 투 포인터로 이도ㅓㅇ
+        # 왼,오 기준에서 작은걸 기준으로 움직이기
+        # 최대 높이랑 비교하면서 차이 계산
         
         left = 0
         right = len(height)-1
-        highest_left = height[left]
-        highest_right = height[right]
+        top_left = height[left]
+        top_right = height[right]
         cnt = 0
-        while left < right:
+        while left<right:
             if height[left] <= height[right]:
                 left += 1
-                if height[left] > highest_left:
-                    highest_left = height[left]
+                if top_left < height[left]:
+                    top_left = height[left]
                 else:
-                    cnt += highest_left - height[left]
+                    cnt += top_left - height[left]
             else:
                 right -= 1
-                # 높이 갱신
-                if height[right] > highest_right:
-                    highest_right = height[right]
+                if top_right < height[right]:
+                    top_right = height[right]
                 else:
-                    cnt += highest_right - height[right]
+                    cnt += top_right - height[right]
         return cnt
 
             
