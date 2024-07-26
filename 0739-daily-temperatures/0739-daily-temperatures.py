fb710f7318c6cc5c87b1558bafb48213d5cbb601
Ye Won Jeong <wonderwall00@naver.com>
2024-07-26 15:32:23 +0900

    Time: 892 ms (54.75%), Space: 30.8 MB (65.56%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,16 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        #나보다 높은 온도를 만나기까지 걸리는 시간은?
-        #스택에 인덱스까지 넣어준다
-        # 인덱스에 나중-지금을 넣어주면서, 
-        # stack[-1]보다 큰 값이 나오면 들어있는걸 pop한다
+        #나보다 높은 온도를 만나기까지 얼마나 걸리는가?
+        # 인덱스를 저장하면서, 
+        # stack끝보다 높은 온도면은 pop
         
         stack = []
-        answer = [0] * len(temperatures)
+        answer = [0]*len(temperatures)
         for day,temp in enumerate(temperatures):
             while stack and stack[-1][1] < temp:
                 prev_day, prev_temp = stack.pop()
                 answer[prev_day] = day - prev_day
             stack.append((day,temp))
         return answer
+                
         
