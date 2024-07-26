57d7a17c0d417e64aa21f5bad18b68393f740daf
Ye Won Jeong <wonderwall00@naver.com>
2024-07-27 01:52:50 +0900

    Time: 363 ms (98.24%), Space: 17.5 MB (33.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,26 @@
 from collections import deque
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #코인을... 누적으로 더해주면서 cnt 출력
+        #코인을 누적으로 더해준다
+        #queue에 지금 합, cnt를 넣는다.
+        # 총량을 찾으면 .cnt 반환한다
+        # 총량보다 적을 때만 append한다
         if amount == 0:
             return 0
         
         queue = deque([(0,0)])
         visited = set()
-        
         while queue:
-            cur_coin, cnt = queue.popleft()
-            if cur_coin == amount:
+            money, cnt = queue.popleft()
+            if money == amount:
                 return cnt
             for i in range(len(coins)):
-                next_coin = cur_coin + coins[i]
-                if next_coin not in visited and next_coin<= amount:
-                    queue.append((next_coin,cnt+1))
-                    visited.add(next_coin)
+                next_money = coins[i] + money
+                if next_money not in visited and next_money <= amount:
+                    queue.append((next_money, cnt+1))
+                    visited.add(next_money)
         return -1
+            
         
                     
         
