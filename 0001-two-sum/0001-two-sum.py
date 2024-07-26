e47133a225fc6f4a21020503cda907a1d31f302a
Ye Won Jeong <wonderwall00@naver.com>
2024-07-27 01:00:20 +0900

    Time: 71 ms (42.24%), Space: 18 MB (7.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 인덱스 출력
-        new_nums = []
-        for i in enumerate(nums):
-            new_nums.append(i)
-        new_nums.sort(key = lambda x : x[1])
+        # 투 포인터
+        nums = [[i,n] for i,n in enumerate(nums)]
+        nums.sort(key= lambda x : x[1])
+
         left = 0
-        right = len(new_nums) -1
-        while left<=right:
-            if new_nums[left][1] + new_nums[right][1] == target:
-                return [new_nums[left][0], new_nums[right][0]]
-            elif new_nums[left][1] + new_nums[right][1] < target:
-                left += 1
-            else:
+        right = len(nums)-1
+        while left < right:
+            if nums[left][1] + nums[right][1] == target:
+                return [nums[left][0], nums[right][0]]
+            elif nums[left][1] + nums[right][1] > target:
                 right -= 1
+            else:
+                left += 1
+        
