cb231e36bd670d58d9383b1db722ef36e55c67e2
Ye Won Jeong <wonderwall00@naver.com>
2024-07-27 11:58:13 +0900

    Time: 177 ms (98.29%), Space: 18.1 MB (27.26%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,17 +1,18 @@
-
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:  
-        # 반복문 활용하기
+    def search(self, nums: List[int], target: int) -> int:
+        
+        #반 쪼갈라서 찾기
+        #이미 정렬 되어있지
         start = 0
         end = len(nums)-1
-        while(start<=end):
+        while start<=end:
             mid = (start+end)//2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                start = mid + 1
+            elif nums[mid] <target:
+                start = mid+1
             else:
-                end = mid - 1
+                end = mid -1
         return -1
-            
+        
         
