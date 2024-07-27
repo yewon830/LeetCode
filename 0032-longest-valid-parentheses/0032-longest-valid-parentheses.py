f21fea33a6efd4033f989287ec822cbde1e838ea
Ye Won Jeong <wonderwall00@naver.com>
2024-07-27 16:30:51 +0900

    Time: 40 ms (72.47%), Space: 17.2 MB (32.29%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,20 +1,21 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        # 가장 긴 유효 괄호 찾기
-        # 가장 긴 유효 괄호 찾는 법 : -1과 잘못된 괄호를 기준점으로 해서 그 간격 재기
-        # 여는 괄호면 append(인덱스를)
-        # 닫는 괄호면 pop
-            # 스택이 있으면 , stack[-1]과 지금 인덱스를 빼고 갱신
-            # 스택이 없으면. 지금 인덱스 append
-        longest = 0
+        # 가장 긴 유효한 괄호 찾기
+        # -1, 그리고 잘못된 괄호를 기준점으로 삼는다.
+        # 여는 괄호면 스택에 일단 인덱스를 넣는다
+        # 닫는 괄호면은 일단 pop한다
+        # pop하는데, 만약에 스택이 있으면 longest를 기준점이랑 인덱스를 빼서 더 큰걸로 업데이트한다
+        # 스택이 없으면 인덱스 어팬드
+        
         stack = [-1]
+        longest = 0
         for i in range(len(s)):
             if s[i] == '(':
                 stack.append(i)
             else:
                 stack.pop()
                 if stack:
-                    longest = max(longest, i - stack[-1])
+                    longest = max(longest, i-stack[-1])
                 else:
                     stack.append(i)
         return longest
