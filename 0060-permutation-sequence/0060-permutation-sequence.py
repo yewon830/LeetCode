804806c02ea315bcf89b451ead1f16a95e7bca29
Ye Won Jeong <wonderwall00@naver.com>
2024-07-27 14:54:12 +0900

    Time: 36 ms (64.32%), Space: 16.6 MB (32.12%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,26 +1,19 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        #전체 순열에서 n번째 순열에 어떤 수가 들어가는지 찾기.
-        # 그러기 위해서는 인덱스가 어디 집단에 있어서 무슨 수가 들어가는지 유추 필요함. 
-        # 1. k // (n-1)! 해서 집단을 알아내고, 그리고 그걸 인덱스에 넣는다
-        # 2. k = k%(n-1)!로 재할당하고, 계속 반복한다. (아웃풋 길이가 n이 될떄까지)
-        
-        #반복문으로 풀기
-        result = []
+#         1. 팩토리얼을 계산한다. k는 하나 뺴준다(인덱스라서)
+# 2. 반복문으로 k//(n-i)를 구하고,
+#    k 에 k%(n-i)값을 할당한다.
         k -= 1
-        nums = [num for num in range(1,n+1)]
+        result = []
+        nums = [i for i in range(1,n+1)]
         def factorial(n):
-            if n==1 or n==0:
+            if n == 1 or n== 0:
                 return 1
             return n * factorial(n-1)
-        i =1
-        while(i<=n):
-            idx = k // factorial(n-i)
-            num  = nums.pop(idx)
-            result.append(num)
+        
+        for i in range(1,n+1):
+            result.append(nums.pop(k//factorial(n-i)))
             k = k % factorial(n-i)
-            i += 1
         
         return ''.join(map(str,result))
-        
-            
+    
