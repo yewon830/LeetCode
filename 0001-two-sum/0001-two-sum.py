f143002ef0d6f82a89147091777164b778ffe208
Ye Won Jeong <wonderwall00@naver.com>
2024-08-06 15:36:27 +0900

    Time: 59 ms (65.57%), Space: 17.8 MB (26.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 투 포인터
-        nums = [[i,n] for i,n in enumerate(nums)]
-        nums.sort(key= lambda x : x[1])
-
-        left = 0
-        right = len(nums)-1
-        while left < right:
-            if nums[left][1] + nums[right][1] == target:
-                return [nums[left][0], nums[right][0]]
-            elif nums[left][1] + nums[right][1] > target:
-                right -= 1
-            else:
-                left += 1
+        # 해시 테이블로 풀기
+        # nums 값:인덱스 인 딕셔너리 추가로 만들기
+        # for문으로 nums을 돌면서, target - 지금요소 가 있는지 딕셔너리에서 확인
+        nums_dict = {}
+        for i,e in enumerate(nums):
+            nums_dict[e] = i
         
+        print(nums_dict)
+        
+        for i in range(len(nums)):
+            if (target-nums[i]) in nums_dict and i !=nums_dict[target-nums[i]]:
+                return [i, nums_dict[target-nums[i]]]
