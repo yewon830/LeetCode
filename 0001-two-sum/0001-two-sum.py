fe3a402c7198475d81e44948ee7ff91c97c8880f
Ye Won Jeong <wonderwall00@naver.com>
2024-08-11 13:48:19 +0900

    Time: 58 ms (69.87%), Space: 17.8 MB (27.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # 해시 테이블로 풀기
-        # nums 값:인덱스 인 딕셔너리 추가로 만들기
-        # for문으로 nums을 돌면서, target - 지금요소 가 있는지 딕셔너리에서 확인
+        # 두 요소 합을 더하면 target이 되어야 한다. 
         nums_dict = {}
         for i,e in enumerate(nums):
+            if target - e in nums_dict:
+                return [i, nums_dict[target-e]]
             nums_dict[e] = i
         
-        print(nums_dict)
-        
-        for i in range(len(nums)):
-            if (target-nums[i]) in nums_dict and i !=nums_dict[target-nums[i]]:
-                return [i, nums_dict[target-nums[i]]]
