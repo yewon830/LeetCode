54ad0c8a46fa63492e9c45130d4b5a129a724885
Ye Won Jeong <wonderwall00@naver.com>
2024-08-28 16:00:04 +0900

    Time: 40 ms (17.23%), Space: 16.5 MB (14.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # 1,2로 n에 도달할 수 있는 방법의 가짓수 구하기
+        # 1,2칸을 +하며 n에 도달할 수 있는 방법의 가짓수 구하기
+        # 점화식: n에 도달하는 방법은 1칸전에 도달하는 경우의 수 + 2칸전에 도달하는 경우의 수
         dp = [0]*(n+1)
-        #초기세팅
         dp[0] = 1
         dp[1] = 1
         for i in range(2,n+1):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
-        
