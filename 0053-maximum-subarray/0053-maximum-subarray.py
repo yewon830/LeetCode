a9e0d6966dfe816a7fbbc5419759066a95d076e3
Ye Won Jeong <wonderwall00@naver.com>
2024-10-10 17:25:25 +0900

    Time: 498 ms (89.11%), Space: 30.9 MB (74.59%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # 합계가 가장 큰 sub 배열 찾기
+        # 완탐은 안될거같음 배열 너무 김.
+        # dp[i]에는 지금까지의 인덱스에서 만들 수 있는 sub 배열의 최대 합이 들어간다. 
+        #   만약에 dp의 이전 값이 음수라면 포함시키지 않는다.(손해)
+        #   만약에 양수면은 포함시킨다.
+        #   dp의 max값을 return한다
+        
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        if len(nums) ==1:
+            return dp[0]
+        
+        for i in range(1,len(nums)):
+            #음수
+            if dp[i-1] <0:
+                dp[i] = nums[i]
+            else:
+                dp[i] = nums[i] + dp[i-1]
+        return max(dp)
+        
